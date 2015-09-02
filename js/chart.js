f393e1ddb19cccbbf42bfc9bb4364de340ef83f8
iennacca <iennacca@yahoo.com>
2015-09-02 18:09:48 -0400

    Fixed padding for labels Fixed bootstrap theme reference

--- js/chart.js
+++ js/chart.js
@@ -5,7 +5,8 @@
 
 var width = 960,
     height = 700,
-    radius = Math.min(width, height) / 2;
+    radius = Math.min(width, height) / 2,
+    padding = 5;
 
 var x = d3.scale.linear()
     .range([0, 2 * Math.PI]);
@@ -52,8 +53,10 @@ svg.call(tip);
 d3.json("data/dmf2.json", function(error, root){
     if (error) throw(error);
 
+    var nodes = partition.nodes(root);
+
     var path = svg.selectAll("path")
-        .data(partition.nodes(root))
+        .data(nodes)
         .enter().append("path")
         .attr("d", arc)
         .style("fill", function (d) {
@@ -62,6 +65,31 @@ d3.json("data/dmf2.json", function(error, root){
         .on("click", click)
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide);
+
+    var text = svg.selectAll("text").data(nodes);
+    var textEnter = text.enter().append("text")
+        .style("fill-opacity", 1)
+        .style("fill", function(d) {
+            return brightness(d3.rgb(colour(d))) < 125 ? "#eee" : "#000";
+        })
+        .attr("text-anchor", function(d) {
+            return x(d.x + d.dx / 2) > Math.PI ? "end" : "start";
+        })
+        .attr("dy", ".2em")
+        .attr("transform", function(d) {
+            var multiline = (d.name || "").split(" ").length > 1,
+                angle = x(d.x + d.dx / 2) * 180 / Math.PI - 90,
+                rotate = angle + (multiline ? -.5 : 0);
+            return "rotate(" + rotate + ")translate(" + (y(d.y) + padding) + ")rotate(" + (angle > 90 ? -180 : 0) + ")";
+        })
+        .on("click", click);
+    textEnter.append("tspan")
+        .attr("x", 0)
+        .text(function(d) { return d.depth ? d.name.split(" ")[0] : ""; });
+    textEnter.append("tspan")
+        .attr("x", 0)
+        .attr("dy", "1em")
+        .text(function(d) { return d.depth ? d.name.split(" ")[1] || "" : ""; });
 
     function click(d) {
         path.transition()
@@ -87,4 +115,21 @@ function arcTween(d) {
             return arc(d);
         };
     };
+}
+
+// http://www.w3.org/WAI/ER/WD-AERT/#color-contrast
+function brightness(rgb) {
+    return rgb.r * .299 + rgb.g * .587 + rgb.b * .114;
+}
+
+function colour(d) {
+    if (d.children) {
+        // There is a maximum of two children!
+        var colours = d.children.map(colour),
+            a = d3.hsl(colours[0]),
+            b = d3.hsl(colours[1]);
+        // L*a*b* might be better here...
+        return d3.hsl((a.h + b.h) / 2, a.s * 1.2, a.l / 1.2);
+    }
+    return d.colour || "#fff";
 }
