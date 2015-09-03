7bbd643941ca73e6d7a2ec9f59547505800d3cc4
iennacca <iennacca@yahoo.com>
2015-09-03 07:11:28 -0400

    Added text label movement

--- js/chart.js
+++ js/chart.js
@@ -1,135 +1,84 @@
-/**
- * Created by jchaves on 9/2/15.
- */
-
 
 var width = 960,
     height = 700,
-    radius = Math.min(width, height) / 2,
-    padding = 5;
+    radius = Math.min(width, height) / 2;
 
 var x = d3.scale.linear()
     .range([0, 2 * Math.PI]);
 
-var y = d3.scale.sqrt()
+var y = d3.scale.linear()
     .range([0, radius]);
 
 var color = d3.scale.category20c();
 
-var svg = d3.select(".chartviewport").append("svg")
+var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
 
 var partition = d3.layout.partition()
-    .value(function (d) {
-        return d.size;
-    });
+    .value(function(d) { return d.size; });
 
 var arc = d3.svg.arc()
-    .startAngle(function (d) {
-        return Math.max(0, Math.min(2 * Math.PI, x(d.x)));
-    })
-    .endAngle(function (d) {
-        return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx)));
-    })
-    .innerRadius(function (d) {
-        return Math.max(0, y(d.y));
-    })
-    .outerRadius(function (d) {
-        return Math.max(0, y(d.y + d.dy));
-    });
+    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
 
-var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-10, 0])
-    .html(function (d) {
-        return "<span style='color:green'>" + d.name + "</span>";
-    });
+d3.json("data/dmf2.json", function(error, root) {
+    var g = svg.selectAll("g")
+        .data(partition.nodes(root))
+        .enter().append("g");
 
-svg.call(tip);
-
-d3.json("data/dmf2.json", function(error, root){
-    if (error) throw(error);
-
-    var nodes = partition.nodes(root);
-
-    var path = svg.selectAll("path")
-        .data(nodes)
-        .enter().append("path")
+    var path = g.append("path")
         .attr("d", arc)
-        .style("fill", function (d) {
-            return d.new ? "green" : color((d.children ? d : d.parent).name);
-        })
-        .on("click", click)
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide);
-
-    var text = svg.selectAll("text").data(nodes);
-    var textEnter = text.enter().append("text")
-        .style("fill-opacity", 1)
-        .style("fill", function(d) {
-            return brightness(d3.rgb(colour(d))) < 125 ? "#eee" : "#000";
-        })
-        .attr("text-anchor", function(d) {
-            return x(d.x + d.dx / 2) > Math.PI ? "end" : "start";
-        })
-        .attr("dy", ".2em")
-        .attr("transform", function(d) {
-            var multiline = (d.name || "").split(" ").length > 1,
-                angle = x(d.x + d.dx / 2) * 180 / Math.PI - 90,
-                rotate = angle + (multiline ? -.5 : 0);
-            return "rotate(" + rotate + ")translate(" + (y(d.y) + padding) + ")rotate(" + (angle > 90 ? -180 : 0) + ")";
-        })
+        .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
         .on("click", click);
-    textEnter.append("tspan")
-        .attr("x", 0)
-        .text(function(d) { return d.depth ? d.name.split(" ")[0] : ""; });
-    textEnter.append("tspan")
-        .attr("x", 0)
-        .attr("dy", "1em")
-        .text(function(d) { return d.depth ? d.name.split(" ")[1] || "" : ""; });
+
+    var text = g.append("text")
+        .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
+        .attr("x", function(d) { return y(d.y); })
+        .attr("dx", "6") // margin
+        .attr("dy", ".35em") // vertical-align
+        .text(function(d) { return d.name; });
 
     function click(d) {
+        // fade out all text elements
+        text.transition().attr("opacity", 0);
+
         path.transition()
             .duration(750)
-            .attrTween("d", arcTween(d));
+            .attrTween("d", arcTween(d))
+            .each("end", function(e, i) {
+                // check if the animated element's data e lies within the visible angle span given in d
+                if (e.x >= d.x && e.x < (d.x + d.dx)) {
+                    // get a selection of the associated text element
+                    var arcText = d3.select(this.parentNode).select("text");
+                    // fade in the text element and recalculate positions
+                    arcText.transition().duration(750)
+                        .attr("opacity", 1)
+                        .attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
+                        .attr("x", function(d) { return y(d.y); });
+                }
+            });
     }
 });
 
-
-//d3.select(self.frameElement).style("height", height + "px");
+d3.select(self.frameElement).style("height", height + "px");
 
 // Interpolate the scales!
 function arcTween(d) {
     var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
         yd = d3.interpolate(y.domain(), [d.y, 1]),
         yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
-    return function (d, i) {
-        return i ? function (t) {
-            return arc(d);
-        } : function (t) {
-            x.domain(xd(t));
-            y.domain(yd(t)).range(yr(t));
-            return arc(d);
-        };
+    return function(d, i) {
+        return i
+            ? function(t) { return arc(d); }
+            : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
     };
 }
 
-// http://www.w3.org/WAI/ER/WD-AERT/#color-contrast
-function brightness(rgb) {
-    return rgb.r * .299 + rgb.g * .587 + rgb.b * .114;
-}
-
-function colour(d) {
-    if (d.children) {
-        // There is a maximum of two children!
-        var colours = d.children.map(colour),
-            a = d3.hsl(colours[0]),
-            b = d3.hsl(colours[1]);
-        // L*a*b* might be better here...
-        return d3.hsl((a.h + b.h) / 2, a.s * 1.2, a.l / 1.2);
-    }
-    return d.colour || "#fff";
+function computeTextRotation(d) {
+    return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
 }
